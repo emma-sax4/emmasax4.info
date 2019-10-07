a6cfdda38c88308cb07ce193605f544686abdb36
Emma Sax <emma.sax4+git@gmail.com>
2019-10-07 11:41:16 -0500

    Adding custom octokit version for new-branch

---
+++ github/octokit_client.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+require 'octokit'
+
+class OctokitClient
+  def client
+    Octokit::Client.new(access_token: github_token)
+  end
+
+  private def github_token
+    YAML.load_file(github_config_file_path)[:github_token]
+  end
+
+  private def github_config_file_path
+    Dir.pwd.scan(/\A\/[\w]*\/[\w]*\//).first << GITHUB_CONFIG_FILE
+  end
+end
+
+# GITHUB_CONFIG_FILE = ".automation/config.yml"
+
+# private def octokit_client
+#   @octokit_client ||= OctokitClient.new.client
+# end
