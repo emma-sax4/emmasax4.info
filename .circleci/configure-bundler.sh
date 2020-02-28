d54c6e940653ed5d6bc8fe251377c5335d8a25a2
Emma Sax <emma.sax@sportsengine.com>
2020-02-27 23:43:53 -0600

    Move the source lines to the next lines

--- .circleci/configure-bundler.sh
+++ .circleci/configure-bundler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "export BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d ' ')" >> $BASH_ENV; source $BASH_ENV
+echo "export BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d ' ')" >> $BASH_ENV
+source $BASH_ENV
 gem install bundler
