891fff61d2f94dc06804d1ef36df706e941614aa
Emma Sax <emma.sax@sportsengine.com>
2020-02-28 10:38:22 -0600

    Try this

--- .circleci/generate-slack-message.sh
+++ .circleci/generate-slack-message.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ -z $CIRCLE_PULL_REQUESTS ]; then # pull request does not exist
   if [ -z $CIRCLE_USERNAME ]; then # committer user does not exist
-    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME on branch \`$CIRCLE_BRANCH\`"
+    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:\`$CIRCLE_BRANCH\`"
   else # committer user does exist
-    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME on branch \`$CIRCLE_BRANCH\` by $CIRCLE_USERNAME"
+    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:\`$CIRCLE_BRANCH\` by $CIRCLE_USERNAME"
   fi
 else # pull request does exist
   PR_NUM=$(echo $CIRCLE_PULL_REQUEST | sed 's/.*\///')
   if [ -z $CIRCLE_USERNAME ]; then # committer user does not exist
-    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME on branch \`$CIRCLE_BRANCH\` in PR #$PR_NUM"
+    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:\`$CIRCLE_BRANCH\` in PR #$PR_NUM"
   else # committer user does exist
-    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME on branch \`$CIRCLE_BRANCH\` in PR $PR_NUM by $CIRCLE_USERNAME"
+    m="Build #$CIRCLE_BUILD_NUM of $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:\`$CIRCLE_BRANCH\` in PR #$PR_NUM by $CIRCLE_USERNAME"
   fi
 fi
 
