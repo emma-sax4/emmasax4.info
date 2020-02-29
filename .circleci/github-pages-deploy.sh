45685ef786241935784a52737987624abfed656a
Emma Sax <emma.sax@sportsengine.com>
2020-02-29 11:22:56 -0600

    Test out what a failing message for a release branch would look like

--- .circleci/github-pages-deploy.sh
+++ .circleci/github-pages-deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
+# git config user.name "$USER_NAME"
+# git config user.email "$USER_EMAIL"
 
-git checkout master
-git pull origin master
+# git checkout master
+# git pull origin master
 
-find . -maxdepth 1 ! -name "site" ! -name ".git" ! -name ".circleci" -exec rm -rf {} \;
-mv ./site/* .
-rm -R ./site/
+# find . -maxdepth 1 ! -name "site" ! -name ".git" ! -name ".circleci" -exec rm -rf {} \;
+# mv ./site/* .
+# rm -R ./site/
 
-git add -fA
-git commit -m "Deploy to emma-sax4/emma-sax4.github.io.git:master via CircleCI"
-PUSH_STATUS=$(git push origin master 2>&1)
+# git add -fA
+# git commit -m "Deploy to emma-sax4/emma-sax4.github.io.git:master via CircleCI"
+# PUSH_STATUS=$(git push origin master 2>&1)
 
-if [[ $PUSH_STATUS == "Everything up-to-date" ]]; then
-  m="Nothing to commit to master. Deploy to GitHub Pages was *skipped*"
-else
+# if [[ $PUSH_STATUS == "Everything up-to-date" ]]; then
+#   m="Nothing to commit to master. Deploy to GitHub Pages was *skipped*"
+# else
   m="Deploy to GitHub Pages was *successful*"
-fi
+# fi
 
 echo "export DEPLOY_MESSAGE='$m'" >> $BASH_ENV
 source $BASH_ENV
