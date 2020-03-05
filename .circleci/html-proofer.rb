81ed71af8ce1af8edb8457fba03adbca148c43f9
Emma Sax <emma.sax@sportsengine.com>
2020-03-05 09:20:32 -0600

    Try this

--- .circleci/html-proofer.rb
+++ .circleci/html-proofer.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
-puts "yes!"
-# require 'html-proofer'
+require 'html-proofer'
 
-# HTMLProofer.check_directory('./_site', {
-#   assume_extension: true,
-#   allow_hash_href: true,
-#   internal_domains: [ /emmasax4.info/ ],
-#   url_ignore: [ /linkedin/, /digikey/ ],
-#   typhoeus: { timeout: 30 }
-# }).run
+HTMLProofer.check_directory('./_site', {
+  assume_extension: true,
+  allow_hash_href: true,
+  internal_domains: [ /emmasax4.info/ ],
+  url_ignore: [ /linkedin/, /digikey/ ],
+  typhoeus: { timeout: 30 }
+}).run
