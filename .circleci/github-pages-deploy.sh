e3a5a35d40584c2461a1fda33ce97161ebe1cece
Emma Sax <emma.sax@sportsengine.com>
2020-03-05 11:43:13 -0600

    Go back to this

--- .circleci/github-pages-deploy.sh
+++ .circleci/github-pages-deploy.sh
@@ -11,7 +11,7 @@ rm -R ./_site/
 
 git add -fA
 git commit -m "Deploy to emma-sax4/emma-sax4.github.io.git:master via CircleCI"
-PUSH_STATUS='notuptodate'  # $(git push origin master 2>&1)
+PUSH_STATUS=$(git push origin master 2>&1)
 
 if [[ $PUSH_STATUS == "Everything up-to-date" ]]; then
   m="Nothing to commit to \`master\` branch. Deploy to GitHub Pages was *skipped*"
