ac1e863fcab29ac549255e817ffc22ea0dbe7a03
Emma Sax <emma.sax4@gmail.com>
2020-03-05 11:51:31 -0600

    Fix broken CircleCI crons (#173)
    Continue to clean up the CircleCI config, since the crons weren't actually broken at all.

--- .circleci/github-pages-deploy.sh
+++ .circleci/github-pages-deploy.sh
@@ -5,9 +5,9 @@ git config user.email "$USER_EMAIL"
 git checkout master
 git pull origin master
 
-find . -maxdepth 1 ! -name "site" ! -name ".git" ! -name ".circleci" -exec rm -rf {} \;
-mv ./site/* .
-rm -R ./site/
+find . -maxdepth 1 ! -name "_site" ! -name ".git" ! -name ".circleci" -exec rm -rf {} \;
+mv ./_site/* .
+rm -R ./_site/
 
 git add -fA
 git commit -m "Deploy to emma-sax4/emma-sax4.github.io.git:master via CircleCI"
