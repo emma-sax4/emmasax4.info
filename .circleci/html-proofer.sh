2543fc38f60caef4e75676f310f5239755399bb4
Emma Sax <emma.sax4+git@gmail.com>
2020-03-05 11:51:31 -0600

    Fix broken CircleCI crons (#173)
    Continue to clean up the CircleCI config, since the crons weren't actually broken at all.

--- .circleci/html-proofer.sh
+++ .circleci/html-proofer.sh
@@ -3,5 +3,5 @@ bundle exec htmlproofer \
   --assume-extension \
   --allow-hash-href \
   --internal-domains /emmasax4.info/ \
-  --url-ignore /linkedin/,/digikey/ \
-  ./site
+  --url-ignore /linkedin/,/digikey/,/maasaimara/ \
+  ./_site/
