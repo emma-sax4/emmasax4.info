ac4415404b0d0acab0eee5a9d9b36e76a569b8ae
Emma Sax <emma.sax@sportsengine.com>
2020-03-05 09:34:53 -0600

    Go back to bash because it's way faster

---
+++ .circleci/html-proofer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bundle exec htmlproofer \
+  --assume-extension \
+  --allow-hash-href \
+  --internal-domains /emmasax4.info/ \
+  --url-ignore /linkedin/,/digikey/,/maasaimara/ \
+  ./_site
--- .circleci/install-html-proofer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GEM=$(gem list html-proofer)
-
-if [ -z $GEM ]; then
-  gem install html-proofer
-fi
