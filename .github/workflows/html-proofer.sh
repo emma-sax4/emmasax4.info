96299d92dbb6e4c7eb0278f0238996a02964f847
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-05-14 09:40:32 -0500

    Change ACCESS_TOKEN to GITHUB_TOKEN (#201)

--- .github/workflows/html-proofer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bundle exec htmlproofer \
-  --assume-extension \
-  --allow-hash-href \
-  --internal-domains /emmasax4.info/ \
-  --url-ignore /linkedin/,/digikey/,/maasaimara/ \
-  --only_4xx \
-  ./_site/
