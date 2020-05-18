46bbe230373afa6c25d298bf2253681207186d64
Emma Sax <emma.sax4+git@gmail.com>
2020-05-18 12:19:09 -0500

    Revert "Clean up these dirs we no longer need"
    This reverts commit dd1710df255b8e0c94b5027cdebe7afe45e4e3ab.

---
+++ js/show-dates.js
@@ -0,0 +1,30 @@
+/*
+This will take all of the dates for the blog posts (written with the class 'date-meta'),
+and will render them in the readers' local time, instead of in UTC.
+
+An incoming date/time will be formatted like this:
+  2019-12-29 03:00:00 +0000
+
+And it will be turned into this (CST):
+  Sat Dec 28 2019 21:00:00 GMT-0600
+
+And written like this:
+  December 28, 2019
+*/
+
+var dates = document.getElementsByClassName('date-meta');
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July','August', 'September', 'October','November', 'December'
+];
+
+for(counter = 0; counter < dates.length; counter++) {
+  var htmlDate = dates[counter];
+  var dateArray = htmlDate.innerHTML.trim().split(/[^0-9]/);
+  var localDate = new Date(
+    Date.UTC(dateArray[0], dateArray[1]-1, dateArray[2], dateArray[3], dateArray[4], dateArray[5])
+  );
+  var day = localDate.getDate();
+  var year = localDate.getFullYear();
+  var monthIndex = localDate.getMonth();
+  htmlDate.innerHTML = monthNames[monthIndex] + ' ' + day + ', ' + year;
+};
