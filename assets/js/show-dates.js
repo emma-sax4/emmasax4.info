30347f99f04f93faaaf4e3892b6f73b9e419b6ef
Emma Sax <emma.sax4@gmail.com>
2020-06-17 01:25:32 -0500

    Here we go

--- assets/js/show-dates.js
+++ assets/js/show-dates.js
@@ -21,16 +21,38 @@ var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
 
 for (counter = 0; counter < dates.length; counter++) {
   var htmlDate = dates[counter];
-  var dateArray = htmlDate.innerHTML.trim().split(/[^0-9]/);
-  var timezone_as_digits = dateArray[dateArray.length - 1];
+  var dateArrayByDigit = htmlDate.innerHTML.trim().split(/[^0-9]/);
+  var dateArrayBySpace = htmlDate.innerHTML.split(' ');
+  var timezoneAsDigit = dateArrayBySpace[dateArrayBySpace.length - 1];
 
-  if (timezone_as_digits == "+0000") { // UTC timezone
+  if (timezoneAsDigit == "+0000") {
+    // Get the local date when it's written as in UTC timezone
     var localDate = new Date(
-      Date.UTC(dateArray[0], dateArray[1]-1, dateArray[2], dateArray[3], dateArray[4], dateArray[5])
+      Date.UTC(
+        dateArrayByDigit[0], dateArrayByDigit[1]-1, dateArrayByDigit[2],
+        dateArrayByDigit[3], dateArrayByDigit[4], dateArrayByDigit[5]
+      )
     );
   } else {
+    // Get the written date in the local timezone (which may not be written timezone)
+    var writtenDate = new Date(
+      dateArrayByDigit[0], dateArrayByDigit[1]-1, dateArrayByDigit[2],
+      dateArrayByDigit[3], dateArrayByDigit[4], dateArrayByDigit[5]
+    );
+
+    // Convert written date in local timezone to UTC timezone
+    var utcDateAsNumbers = writtenDate.getTime() - (timezoneAsDigit * 60 * 60 * 10);
+    var utcDate = new Date(utcDateAsNumbers);
+    var utcYear = utcDate.getFullYear();
+    var utcMonth = utcDate.getMonth();
+    var utcDay = utcDate.getDate();
+    var utcHour = utcDate.getHours();
+    var utcMinute = utcDate.getMinutes();
+    var utcMilliseconds = utcDate.getMilliseconds();
+
+    // Get the local date when it's written as in UTC timezone
     var localDate = new Date(
-      dateArray[0], dateArray[1]-1, dateArray[2], dateArray[3], dateArray[4], dateArray[5]
+      Date.UTC(utcYear, utcMonth, utcDay, utcHour, utcMinute, utcMilliseconds)
     );
   };
 
