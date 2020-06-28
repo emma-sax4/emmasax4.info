38c6b7050130dbbb4342c721804faa4c7d180b5d
Emma Sax <emma.sax4@gmail.com>
2020-06-27 20:32:31 -0500

    Try this

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -5,28 +5,24 @@ github_repo=$2
 ref=$3
 run_id=$4
 
-if [ $workflow = "develop" ]; then
+if [[ $workflow == develop ]]; then
   head_ref=$5
-  echo "Develop"
   pull_id=$(echo $ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+
   echo "::set-env name=PULL_ID::$pull_id"
   echo "::set-env name=PULL_URL::$(echo https://github.com/$github_repo/pull/$pull_id)"
   echo "::set-env name=BRANCH::$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')"
-elif [ $workflow = "release" ]; then
+elif [[ $workflow == release ]]; then
   github_actor=$5
-  echo "Release workflow"
 
-  if [[ $github_actor = 'pr-scheduler[bot]' ]]; then
-    echo "pr Scheduler"
+  if [[ $github_actor == "pr-scheduler[bot]" ]]; then
     echo "::set-env name=ACTOR_ICON::https://i.imgur.com/tmdeggv.png"
   else
-    echo "not PR scheduler"
     echo "::set-env name=ACTOR_ICON::https://github.com/$github_actor.png"
   fi
 fi
 
-if [ $workflow = "release" ] || [ $workflow = "cron" ]; then
-  echo "Release workflow or Cron workflow!"
+if [[ $workflow == release ]] || [[ $workflow == cron ]]; then
   echo "::set-env name=BRANCH::$(echo $ref | sed -E 's|refs/[a-zA-Z]+/||')"
 fi
 
