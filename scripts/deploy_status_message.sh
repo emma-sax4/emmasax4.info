443f09658f9b04c5be49c219bf962f7488c84dd3
Emma Sax <emma.sax4@gmail.com>
2020-06-27 20:26:33 -0500

    Pass in this too

--- scripts/deploy_status_message.sh
+++ scripts/deploy_status_message.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ ${{ env.DEPLOYMENT_STATUS }} == skipped ]]; then
+status=$1
+
+if [[ $status == skipped ]]; then
   echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *skipped*)"
-elif [[ ${{ env.DEPLOYMENT_STATUS }} == success ]]; then
+elif [[ $status == success ]]; then
   echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *successful*)"
-elif [[ ${{ env.DEPLOYMENT_STATUS }} == failed ]]; then
+elif [[ $status == failed ]]; then
   echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *unsuccessful*)"
 fi
