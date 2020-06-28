6edaded179ed8f95482c3ce35e6b0050b8aa339c
Emma Sax <emma.sax4@gmail.com>
2020-06-27 20:25:49 -0500

    Pass in variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 workflow=$1
+github_repo=$2
+ref=$3
+run_id=$4
 
 if [ $workflow = "develop" ]; then
+  head_ref=$5
   echo "Develop"
-  pull_id=$(echo ${{ github.ref }} | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+  pull_id=$(echo $ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
   echo "::set-env name=PULL_ID::$pull_id"
-  echo "::set-env name=PULL_URL::$(echo https://github.com/${{ github.repository }}/pull/$pull_id)"
-  echo "::set-env name=BRANCH::$(echo ${{ github.head_ref }} | sed -E 's|refs/[a-zA-Z]+/||')"
+  echo "::set-env name=PULL_URL::$(echo https://github.com/$github_repo/pull/$pull_id)"
+  echo "::set-env name=BRANCH::$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')"
 elif [ $workflow = "release" ]; then
+  github_actor=$5
   echo "Release workflow"
-  if [[ ${{ github.actor }} = 'pr-scheduler[bot]' ]]; then
+
+  if [[ $github_actor = 'pr-scheduler[bot]' ]]; then
     echo "pr Scheduler"
     echo "::set-env name=ACTOR_ICON::https://i.imgur.com/tmdeggv.png"
   else
     echo "not PR scheduler"
-    echo "::set-env name=ACTOR_ICON::https://github.com/${{ github.actor }}.png"
+    echo "::set-env name=ACTOR_ICON::https://github.com/$github_actor.png"
   fi
 fi
 
 if [ $workflow = "release" ] || [ $workflow = "cron" ]; then
   echo "Release workflow or Cron workflow!"
-  echo "::set-env name=BRANCH::$(echo ${{ github.ref }} | sed -E 's|refs/[a-zA-Z]+/||')"
-
+  echo "::set-env name=BRANCH::$(echo $ref | sed -E 's|refs/[a-zA-Z]+/||')"
 fi
 
-echo "::set-env name=BUILD_URL::$(echo https://github.com/${{ github.repository }}/actions/runs/${{ github.run_id }})"
+echo "::set-env name=BUILD_URL::$(echo https://github.com/$github_repo/actions/runs/$run_id)"
