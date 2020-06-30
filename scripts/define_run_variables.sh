3ff29c1fc6e6167f99ea912309be1b0848a66caf
Emma Sax <emma.sax4@gmail.com>
2020-06-29 19:03:59 -0500

    Set these back now

--- scripts/define_run_variables.sh
+++ scripts/define_run_variables.sh
@@ -12,10 +12,8 @@ if [[ $actions_workflow == develop ]]; then
   echo "::set-env name=PULL_ID::$pull_id"
   echo "::set-env name=PULL_URL::$(echo https://github.com/$github_repo/pull/$pull_id)"
   echo "::set-env name=BRANCH::$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')"
-  echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *failed*)"
 elif [[ $actions_workflow == release ]]; then
   github_actor=$5
-  echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *skipped*)"
 
   if [[ $github_actor == "pr-scheduler[bot]" ]]; then
     echo "::set-env name=ACTOR_ICON::https://i.imgur.com/tmdeggv.png"
@@ -26,6 +24,7 @@ fi
 
 if [[ $actions_workflow == release ]] || [[ $actions_workflow == cron ]]; then
   echo "::set-env name=BRANCH::$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')"
+  echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *skipped*)"
 fi
 
 echo "::set-env name=BUILD_URL::$(echo https://github.com/$github_repo/actions/runs/$actions_run_id)"
