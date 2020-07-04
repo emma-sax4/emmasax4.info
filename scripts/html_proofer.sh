2f727bc9065f250afa4860ee7a3b160b668cac93
Emma Sax <emma.sax4@gmail.com>
2020-07-03 23:22:57 -0500

    Just ignore all of these

--- scripts/html_proofer.sh
+++ scripts/html_proofer.sh
@@ -4,5 +4,9 @@ bundle exec htmlproofer \
   --assume-extension \
   --allow-hash-href \
   --internal-domains /emmasax4.info/ \
-  --url_ignore /www.linkedin.com/ \
+  --url_ignore /linkedin, getitwriteonline, sopalodges/ \
   ./_site/
+
+# https://www.sopalodges.com/lake-naivasha-sopa-resort/the-resort
+# https://getitwriteonline.com/articles/en-dashes-em-dashes/
+# https://www.linkedin.com/in/emmasax4
