eab34db138f085b3a1fe230f5b0fc7a35e22334b
Emma Sax <emma.sax4@gmail.com>
2020-07-03 23:32:07 -0500

    Here we go

--- scripts/html_proofer.sh
+++ scripts/html_proofer.sh
@@ -4,9 +4,10 @@ bundle exec htmlproofer \
   --assume-extension \
   --allow-hash-href \
   --internal-domains /emmasax4.info/ \
-  --url_ignore /linkedin, getitwriteonline, sopalodges/ \
+  --url_ignore /linkedin/,/getitwriteonline/,/sopalodges/,/maasaimara/ \
   ./_site/
 
 # https://www.sopalodges.com/lake-naivasha-sopa-resort/the-resort
 # https://getitwriteonline.com/articles/en-dashes-em-dashes/
 # https://www.linkedin.com/in/emmasax4
+# https://www.maasaimara.com/entries/fig-tree-camp
