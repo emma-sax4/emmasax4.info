8c0a7eecad76ace369bd210b9191fd89bfe94adb
Emma Sax <emma.sax4@gmail.com>
2020-07-04 00:00:05 -0500

    Add more LEGOs! (#277)
    merged by PR Scheduler

--- scripts/html_proofer.sh
+++ scripts/html_proofer.sh
@@ -4,5 +4,10 @@ bundle exec htmlproofer \
   --assume-extension \
   --allow-hash-href \
   --internal-domains /emmasax4.info/ \
-  --only_4xx \
+  --url_ignore /linkedin/,/getitwriteonline/,/sopalodges/,/maasaimara/ \
   ./_site/
+
+# https://www.sopalodges.com/lake-naivasha-sopa-resort/the-resort
+# https://getitwriteonline.com/articles/en-dashes-em-dashes/
+# https://www.linkedin.com/in/emmasax4
+# https://www.maasaimara.com/entries/fig-tree-camp
