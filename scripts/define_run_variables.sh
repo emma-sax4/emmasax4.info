06c888df4c1fcecb790fafa473dbf3ed1e5280d8
Emma Sax <emma.sax4+git@gmail.com>
2020-10-09 21:52:40 -0500

    Update gems; address typos; address GitHub Actions deprecation (#314)
    * Address typos
    
    * Update gems
    
    * Address env variable deprecation

--- scripts/define_run_variables.sh
+++ scripts/define_run_variables.sh
@@ -9,22 +9,22 @@ if [[ $actions_workflow == develop ]]; then
   head_ref=$5
   pull_id=$(echo $github_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
 
-  echo "::set-env name=PULL_ID::$pull_id"
-  echo "::set-env name=PULL_URL::$(echo https://github.com/$github_repo/pull/$pull_id)"
-  echo "::set-env name=BRANCH::$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')"
+  echo "PULL_ID=$pull_id" >> $GITHUB_ENV
+  echo "PULL_URL=https://github.com/$github_repo/pull/$pull_id" >> $GITHUB_ENV
+  echo "BRANCH=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')" >> $GITHUB_ENV
 elif [[ $actions_workflow == release ]]; then
   github_actor=$5
-  echo "::set-env name=DEPLOY_MESSAGE::$(echo Deploy to GitHub Pages was *skipped*)"
+  echo "DEPLOY_MESSAGE=Deploy to GitHub Pages was *skipped*" >> $GITHUB_ENV
 
   if [[ $github_actor == "pr-scheduler[bot]" ]]; then
-    echo "::set-env name=ACTOR_ICON::https://i.imgur.com/tmdeggv.png"
+    echo "ACTOR_ICON=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
   else
-    echo "::set-env name=ACTOR_ICON::https://github.com/$github_actor.png"
+    echo "ACTOR_ICON=https://github.com/$github_actor.png" >> $GITHUB_ENV
   fi
 fi
 
 if [[ $actions_workflow == release ]] || [[ $actions_workflow == cron ]]; then
-  echo "::set-env name=BRANCH::$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')"
+  echo "BRANCH=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')" >> $GITHUB_ENV
 fi
 
-echo "::set-env name=BUILD_URL::$(echo https://github.com/$github_repo/actions/runs/$actions_run_id)"
+echo "BUILD_URL=https://github.com/$github_repo/actions/runs/$actions_run_id" >> $GITHUB_ENV
