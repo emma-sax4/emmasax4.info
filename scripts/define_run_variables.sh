97f1540654c76448ed56948e29f26fcb32a504d3
Emma Sax <emma.sax4+git@gmail.com>
2020-10-27 13:53:35 -0500

    Rename workflows and condense cron workflow (#319)
    * Rename workflows
    
    * Try to condense the cron workflow with the other workflow
    
    * Try this
    
    * Maybe these spaces are needed?
    
    * Try this
    
    * Put all of this back
    
    * Delete this file
    
    * Add an additional cron for testing the cron workflows

--- scripts/define_run_variables.sh
+++ scripts/define_run_variables.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-actions_workflow=$1
-github_repo=$2
-github_ref=$3
-actions_run_id=$4
+args="$*"
+deploy=$(echo "$args" | grep -o 'deploy=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#') # 'true' or 'false'
+github_repo=$(echo "$args" | grep -o 'github_repo=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
+actions_run_id=$(echo "$args" | grep -o 'actions_run_id=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
+github_actor=$(echo "$args" | grep -o 'github_actor=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
+github_ref=$(echo "$args" | grep -o 'github_ref=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
+head_ref=$(echo "$args" | grep -o 'head_ref=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
+build_url="https://github.com/$github_repo/actions/runs/$actions_run_id"
 
-if [[ $actions_workflow == develop ]]; then
-  head_ref=$5
-  pull_id=$(echo $github_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+if [[ $head_ref == "" ]]; then # branch is 'main'
+  branch=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')
+  echo "branch=$branch" >> $GITHUB_ENV
+  echo "build_message=Build <$build_url|$actions_run_id> on branch `$branch`" >> $GITHUB_ENV
+  echo "deploy_message=Deploy to GitHub Pages was *skipped*" >> $GITHUB_ENV
 
-  echo "PULL_ID=$pull_id" >> $GITHUB_ENV
-  echo "PULL_URL=https://github.com/$github_repo/pull/$pull_id" >> $GITHUB_ENV
-  echo "BRANCH=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')" >> $GITHUB_ENV
-elif [[ $actions_workflow == release ]]; then
-  github_actor=$5
-  echo "DEPLOY_MESSAGE=Deploy to GitHub Pages was *skipped*" >> $GITHUB_ENV
+  if [[ $deploy == "true" ]]; then
+    echo "actor_name=$github_actor" >> $GITHUB_ENV
 
-  if [[ $github_actor == "pr-scheduler[bot]" ]]; then
-    echo "ACTOR_ICON=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
-  else
-    echo "ACTOR_ICON=https://github.com/$github_actor.png" >> $GITHUB_ENV
+    if [[ $github_actor == "pr-scheduler[bot]" ]]; then
+      echo "actor_icon=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
+    else
+      echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
+    fi
+  else # this is a cron
+    echo "actor_name=github-actions[bot]" >> $GITHUB_ENV
+    echo "actor_icon=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
   fi
+else
+  pull_id=$(echo $github_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+  pull_url="https://github.com/$github_repo/pull/$pull_id"
+  branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||') # this comes to '' if on 'main' branch
+  echo "branch=$branch" >> $GITHUB_ENV
+  echo "build_message=Build <$build_url|$actions_run_id> on branch `$branch` in PR <$pull_url|#$pull_id>" >> $GITHUB_ENV
+  echo "actor_name=$github_actor" >> $GITHUB_ENV
+  echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
 fi
-
-if [[ $actions_workflow == release ]] || [[ $actions_workflow == cron ]]; then
-  echo "BRANCH=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')" >> $GITHUB_ENV
-fi
-
-echo "BUILD_URL=https://github.com/$github_repo/actions/runs/$actions_run_id" >> $GITHUB_ENV
