f261283bba1047ce6c796edf1c11eac705febe2a
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-27 18:18:22 -0500

    Combine the workflows all into one

--- scripts/define_run_variables.sh
+++ scripts/define_run_variables.sh
@@ -11,9 +11,9 @@ build_url="https://github.com/$github_repo/actions/runs/$actions_run_id"
 
 if [[ $head_ref == "" ]]; then # branch is 'main'
   branch=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')
-  echo "branch=$branch" >> $GITHUB_ENV
+
   echo "build_message=Build <$build_url|$actions_run_id> on branch \`$branch\`" >> $GITHUB_ENV
-  echo "deploy_message=Deploy to GitHub Pages was *skipped*" >> $GITHUB_ENV
+  echo "deploy_message=$(cat deploy-status-message.txt)" >> $GITHUB_ENV
 
   if [[ $deploy == "true" ]]; then
     echo "actor_name=$github_actor" >> $GITHUB_ENV
@@ -30,8 +30,8 @@ if [[ $head_ref == "" ]]; then # branch is 'main'
 else
   pull_id=$(echo $github_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
   pull_url="https://github.com/$github_repo/pull/$pull_id"
-  branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||') # this comes to '' if on 'main' branch
-  echo "branch=$branch" >> $GITHUB_ENV
+  branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')
+
   echo "build_message=Build <$build_url|$actions_run_id> on branch \`$branch\` in PR <$pull_url|#$pull_id>" >> $GITHUB_ENV
   echo "actor_name=$github_actor" >> $GITHUB_ENV
   echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
