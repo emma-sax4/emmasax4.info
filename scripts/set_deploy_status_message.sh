3c1c652ceed9626f7e80013b73752755e8193881
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-27 18:54:30 -0500

    Set one slack message for the build, one for the deploy

--- scripts/set_deploy_status_message.sh
+++ scripts/set_deploy_status_message.sh
@@ -3,9 +3,9 @@
 deploy_status=$1
 
 if [[ $deploy_status == skipped ]]; then
-  echo "Deploy to GitHub Pages was *skipped*." > deploy-status-message.txt
+  echo "deploy_message=Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
 elif [[ $deploy_status == success ]]; then
-  echo "Deploy to GitHub Pages was *successful*." > deploy-status-message.txt
+  echo "deploy_message=Deploy to GitHub Pages was *successful*." >> $GITHUB_ENV
 elif [[ $deploy_status == failed ]]; then
-  echo "Deploy to GitHub Pages was *unsuccessful*." > deploy-status-message.txt
+  echo "deploy_message=Deploy to GitHub Pages was *unsuccessful*." >> $GITHUB_ENV
 fi
--- scripts/set_workflow_variables.sh
+++ scripts/set_workflow_variables.sh
@@ -9,29 +9,30 @@ github_ref=$(echo "$args" | grep -o 'github_ref=[^[:blank:]]*' | sed -e 's#.*=\(
 head_ref=$(echo "$args" | grep -o 'head_ref=[^[:blank:]]*' | sed -e 's#.*=\(\)#\1#')
 build_url="https://github.com/$github_repo/actions/runs/$actions_run_id"
 
-echo "deploy_message=$(cat deploy-status-message.txt)" >> $GITHUB_ENV
+echo "deploy_message=Deploy to GitHub Pages was *skipped*" >> $GITHUB_ENV
 
-if [[ $github_ref == 'refs/heads'* ]]; then # this is a branch, not a pull request
-  branch=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')
-  echo "build_message=Build <$build_url|$actions_run_id> on branch \`$branch\`" >> $GITHUB_ENV
-
-  if [[ $event_name == "push" ]]; then # this is a merge and a deploy
-    echo "actor_name=$github_actor" >> $GITHUB_ENV
-
-    if [[ $github_actor == "pr-scheduler[bot]" ]]; then
-      echo "actor_icon=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
-    else
-      echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
-    fi
-  else # this is a cron
-    echo "actor_name=github-actions[bot]" >> $GITHUB_ENV
-    echo "actor_icon=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
-  fi
-else
+if [[ $github_ref == 'refs/pull'* ]]; then # this is a pull request
   pull_id=$(echo $github_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
   pull_url="https://github.com/$github_repo/pull/$pull_id"
+  build_message_addition=" in PR <$pull_url|#$pull_id>"
   branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')
-  echo "build_message=Build <$build_url|$actions_run_id> on branch \`$branch\` in PR <$pull_url|#$pull_id>" >> $GITHUB_ENV
+else
+  build_message_addition=""
+  branch=$(echo $github_ref | sed -E 's|refs/[a-zA-Z]+/||')
+fi
+
+echo "branch=$branch" >> $GITHUB_ENV
+echo "build_message=Build <$build_url|$actions_run_id> on branch \`$branch\`$build_message_addition" >> $GITHUB_ENV
+
+if [[ $event_name == "schedule" ]]; then # this is a cron
+  echo "actor_name=github-actions[bot]" >> $GITHUB_ENV
+  echo "actor_icon=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
+else
   echo "actor_name=$github_actor" >> $GITHUB_ENV
-  echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
+
+  if [[ $github_actor == "pr-scheduler[bot]" ]]; then
+    echo "actor_icon=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
+  else
+    echo "actor_icon=https://github.com/$github_actor.png" >> $GITHUB_ENV
+  fi
 fi
