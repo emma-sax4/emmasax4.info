b16be4b556aedcf8c5aeae8f0e3433e54e249d8d
Emma Sax <emma.sax4+git@gmail.com>
2020-10-28 15:46:32 -0500

    Play with merging all jobs into one (#323)
    * Attempt to merge jobs into one
    
    * Modify
    
    * Try this to set an empty value
    
    * Remove the artifact uploading
    
    * Remove these if cases for testing
    
    * Try this?
    
    * Set this for testing
    
    * Try this to see if it updates
    
    * Capitalize this
    
    * Oopsy
    
    * Put the if cases back
    
    * Remove the default message line
    
    * Switch this order
    
    * Rename workflow

--- scripts/set_workflow_environment_variables.sh
+++ scripts/set_workflow_environment_variables.sh
@@ -5,34 +5,37 @@ args="$*"
 event_name=$(echo $args | cut -d " " -f 1)
 repository=$(echo $args | cut -d " " -f 2)
 run_id=$(echo $args | cut -d " " -f 3)
-gh_actor=$(echo $args | cut -d " " -f 4)
-gh_ref=$(echo $args | cut -d " " -f 5)
+author=$(echo $args | cut -d " " -f 4)
+ref=$(echo $args | cut -d " " -f 5)
 head_ref=$(echo $args | cut -d " " -f 6)
 
-build_url="https://github.com/$repository/actions/runs/$run_id"
-
-if [[ $gh_ref == 'refs/pull'* ]]; then # this is a pull request
-  pull_id=$(echo $gh_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
-  pull_url="https://github.com/$repository/pull/$pull_id"
-  build_message_addition=" in PR <$pull_url|#$pull_id>"
+if [[ $ref == 'refs/pull'* ]]; then # this is a pull request
+  pull_id=$(echo $ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+  build_message_addition=" in PR <https://github.com/$repository/pull/$pull_id|#$pull_id>"
   branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')
 else
   build_message_addition=""
-  branch=$(echo $gh_ref | sed -E 's|refs/[a-zA-Z]+/||')
+  branch=$(echo $ref | sed -E 's|refs/[a-zA-Z]+/||')
 fi
 
-echo "branch=$branch" >> $GITHUB_ENV
-echo "build_message=Build <$build_url|$run_id> on branch \`$branch\`$build_message_addition" >> $GITHUB_ENV
+echo "BRANCH=$branch" >> $GITHUB_ENV
+echo "BUILD_MESSAGE=Build <https://github.com/$repository/actions/runs/$run_id|$run_id> on branch \`$branch\`$build_message_addition" >> $GITHUB_ENV
 
 if [[ $event_name == "schedule" ]]; then # this is a cron
-  echo "author_name=github-actions[bot]" >> $GITHUB_ENV
-  echo "author_icon=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
+  echo "AUTHOR_NAME=github-actions[bot]" >> $GITHUB_ENV
+  echo "AUTHOR_ICON=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
 else
-  echo "author_name=$gh_actor" >> $GITHUB_ENV
+  echo "AUTHOR_NAME=$author" >> $GITHUB_ENV
 
-  if [[ $gh_actor == "pr-scheduler[bot]" ]]; then
-    echo "author_icon=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
+  if [[ $author == "pr-scheduler[bot]" ]]; then
+    echo "AUTHOR_ICON=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
   else
-    echo "author_icon=https://github.com/$gh_actor.png" >> $GITHUB_ENV
+    echo "AUTHOR_ICON=https://github.com/$author.png" >> $GITHUB_ENV
   fi
+fi
+
+if [[ $event_name == "push" ]] && [[ $ref == "refs/heads/main" ]]; then # this is a push commit to the 'main' branch
+  echo "DEPLOY_MESSAGE= Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
+else
+  echo "DEPLOY_MESSAGE=" >> $GITHUB_ENV
 fi
