12f72aa65b5420a2e1b3ec1e68c7059f2ddf4838
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-28 08:51:20 -0500

    Try this to set an empty value

--- scripts/set_workflow_environment_variables.sh
+++ scripts/set_workflow_environment_variables.sh
@@ -37,5 +37,5 @@ fi
 if [[ $event_name == "push" ]] && [[ $ref == "refs/heads/main" ]]; then # push commits to the 'main' branch
   echo "deploy_message= Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
 else
-  echo "deploy_message=''" >> $GITHUB_ENV
+  echo "deploy_message=" >> $GITHUB_ENV
 fi
