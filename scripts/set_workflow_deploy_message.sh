acee91209ed07bbf59e75fe9eecbd8bd3824985d
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-28 08:44:28 -0500

    Attempt to merge jobs into one

--- scripts/set_workflow_deploy_message.sh
+++ scripts/set_workflow_deploy_message.sh
@@ -3,9 +3,9 @@
 deploy_status=$1
 
 if [[ $deploy_status == skipped ]]; then
-  echo "deploy_message=Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
+  echo "deploy_message= Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
 elif [[ $deploy_status == success ]]; then
-  echo "deploy_message=Deploy to GitHub Pages was *successful*." >> $GITHUB_ENV
+  echo "deploy_message= Deploy to GitHub Pages was *successful*." >> $GITHUB_ENV
 elif [[ $deploy_status == failed ]]; then
-  echo "deploy_message=Deploy to GitHub Pages was *unsuccessful*." >> $GITHUB_ENV
+  echo "deploy_message= Deploy to GitHub Pages was *unsuccessful*." >> $GITHUB_ENV
 fi
--- scripts/set_workflow_environment_variables.sh
+++ scripts/set_workflow_environment_variables.sh
@@ -5,34 +5,38 @@ args="$*"
 event_name=$(echo $args | cut -d " " -f 1)
 repository=$(echo $args | cut -d " " -f 2)
 run_id=$(echo $args | cut -d " " -f 3)
-gh_actor=$(echo $args | cut -d " " -f 4)
-gh_ref=$(echo $args | cut -d " " -f 5)
+author=$(echo $args | cut -d " " -f 4)
+ref=$(echo $args | cut -d " " -f 5)
 head_ref=$(echo $args | cut -d " " -f 6)
 
-build_url="https://github.com/$repository/actions/runs/$run_id"
-
-if [[ $gh_ref == 'refs/pull'* ]]; then # this is a pull request
-  pull_id=$(echo $gh_ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
+if [[ $ref == 'refs/pull'* ]]; then # this is a pull request
+  pull_id=$(echo $ref | sed -E 's|refs/pull/||' | sed -E 's|/merge||')
   pull_url="https://github.com/$repository/pull/$pull_id"
   build_message_addition=" in PR <$pull_url|#$pull_id>"
   branch=$(echo $head_ref | sed -E 's|refs/[a-zA-Z]+/||')
 else
   build_message_addition=""
-  branch=$(echo $gh_ref | sed -E 's|refs/[a-zA-Z]+/||')
+  branch=$(echo $ref | sed -E 's|refs/[a-zA-Z]+/||')
 fi
 
 echo "branch=$branch" >> $GITHUB_ENV
-echo "build_message=Build <$build_url|$run_id> on branch \`$branch\`$build_message_addition" >> $GITHUB_ENV
+echo "build_message=Build <https://github.com/$repository/actions/runs/$run_id|$run_id> on branch \`$branch\`$build_message_addition" >> $GITHUB_ENV
 
 if [[ $event_name == "schedule" ]]; then # this is a cron
   echo "author_name=github-actions[bot]" >> $GITHUB_ENV
   echo "author_icon=https://i.imgur.com/kUxzV44s.png" >> $GITHUB_ENV
 else
-  echo "author_name=$gh_actor" >> $GITHUB_ENV
+  echo "author_name=$author" >> $GITHUB_ENV
 
-  if [[ $gh_actor == "pr-scheduler[bot]" ]]; then
+  if [[ $author == "pr-scheduler[bot]" ]]; then
     echo "author_icon=https://i.imgur.com/tmdeggv.png" >> $GITHUB_ENV
   else
-    echo "author_icon=https://github.com/$gh_actor.png" >> $GITHUB_ENV
+    echo "author_icon=https://github.com/$author.png" >> $GITHUB_ENV
   fi
+fi
+
+if [[ $event_name == "push" ]] && [[ $ref == "refs/heads/main" ]]; then # push commits to the 'main' branch
+  echo "deploy_message= Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
+else
+  echo "deploy_message=''" >> $GITHUB_ENV
 fi
