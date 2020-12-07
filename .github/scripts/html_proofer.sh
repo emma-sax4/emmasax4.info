fe78199e334c7389a1df7b3d1fd9d0ac57265709
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-12-07 09:32:32 -0600

    Add codepen to the list of URLs for HTML proofer to ignore

--- .github/scripts/html_proofer.sh
+++ .github/scripts/html_proofer.sh
@@ -4,10 +4,11 @@ bundle exec htmlproofer \
   --assume-extension \
   --allow-hash-href \
   --internal-domains /emmasax4.com/ \
-  --url_ignore /linkedin/,/getitwriteonline/,/sopalodges/,/maasaimara/ \
+  --url_ignore /linkedin/,/getitwriteonline/,/sopalodges/,/maasaimara/,/codepen.io/ \
   _site
 
 # https://www.sopalodges.com/lake-naivasha-sopa-resort/the-resort
 # https://getitwriteonline.com/articles/en-dashes-em-dashes/
 # https://www.linkedin.com/in/emmasax4
 # https://www.maasaimara.com/entries/fig-tree-camp
+# https://codepen.io/Paulie-D/pen/zvkpJ/
