1cb89dc2651ab19cd835ab47abfe5547c8004619
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-12-10 08:51:15 -0600

    Quote these as well

--- .github/scripts/set_deploy_message.sh
+++ .github/scripts/set_deploy_message.sh
@@ -2,10 +2,10 @@
 
 deploy_status=$1
 
-if [[ $deploy_status == skipped ]]; then
+if [[ $deploy_status == "skipped" ]]; then
   echo "DEPLOY_MESSAGE= Deploy to GitHub Pages was *skipped*." >> $GITHUB_ENV
-elif [[ $deploy_status == success ]]; then
+elif [[ $deploy_status == "success" ]]; then
   echo "DEPLOY_MESSAGE= Deploy to GitHub Pages was *successful*." >> $GITHUB_ENV
-elif [[ $deploy_status == failed ]]; then
+elif [[ $deploy_status == "failed" ]]; then
   echo "DEPLOY_MESSAGE= Deploy to GitHub Pages was *unsuccessful*." >> $GITHUB_ENV
 fi
