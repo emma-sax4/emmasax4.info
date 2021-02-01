c653cdd5004ce573edb5ccbda992f76bf0f5ba8f
Emma Sax <emma.sax4+git@gmail.com>
2021-01-31 20:30:05 -0600

    Even more spam protection (#360)
    merged by PR Scheduler

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,4 +1,5 @@
 var submitted=false;
+var code="na7iKQolB9SFbmOCe19NPi82mHPY4ILTbQ9QR4PxHIr5SIl7p5L8Ta9ZSppZ3HHS";
 
 function showFormResponse() {
   document.getElementById("formResponse").style.display = "block";
@@ -6,9 +7,11 @@ function showFormResponse() {
 };
 
 function recaptchaCallback() {
-  $('#submitButton').removeAttr('disabled');
+  $("#submitButton").removeAttr("disabled");
+  document.getElementById("verification").value = code;
 };
 
 function recaptchaExpiredCallback() {
-  $('#submitButton').attr('disabled', true);
+  $("#submitButton").attr("disabled", true);
+  document.getElementById("verification").value = null;
 };
