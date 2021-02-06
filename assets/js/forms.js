c0c427dd6658e7275421df022fb93d0d086b2dae
Emma Sax <emma.sax4+git@gmail.com>
2021-02-06 11:39:26 -0600

    Fix up the contact page so its display: none are classes

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -3,7 +3,7 @@ var recaptchaCompleted = false;
 var code = "na7iKQolB9SFbmOCe19NPi82mHPY4ILTbQ9QR4PxHIr5SIl7p5L8Ta9ZSppZ3HHS";
 
 function showFormResponse() {
-  document.getElementById("formResponse").style.display = "block";
+  document.getElementById("formResponse").classList.remove("invisible");
   document.getElementById("form").remove();
 };
 
