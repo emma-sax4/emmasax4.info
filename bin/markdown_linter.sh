e64115f7dc91ffc664386cd945aba1cddb828d20
Emma Sax <emma.sax4+git@gmail.com>
2021-03-17 19:21:39 -0500

    See what happens if this breaks

--- bin/markdown_linter.sh
+++ bin/markdown_linter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bundle exec mdl -i \
-  -r ~MD013,~MD002,~MD033,~MD026,~MD029,~MD046,~MD001,~MD024,~MD034 \
+  -r ~MD013,~MD002,~MD033,~MD026,~MD029,~MD046,~MD001,~MD024 \
   README.md \
   .github \
   pages \
