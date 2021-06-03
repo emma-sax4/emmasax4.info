384afaa02d742361a4ddecac269f23dbdd10b37b
Emma Sax <emma.sax4+git@gmail.com>
2021-06-02 22:35:19 -0500

    Name the <a> tag 'photoswipe'

--- assets/js/activate_photoswipe.js
+++ assets/js/activate_photoswipe.js
@@ -2,7 +2,7 @@ import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js'
 
 const lightbox = new PhotoSwipeLightbox({
   gallerySelector: '.photoswipe-gallery',
-  childSelector: '.photoswipe-gallery',
+  childSelector: '.photoswipe',
   pswpModule: '/assets/js/photoswipe.esm.min.js',
   pswpCSS: '/assets/css/photoswipe.min.css'
 })
