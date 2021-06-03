673b06ce85dff24f646ece75aa94397e065713ce
Emma Sax <emma.sax4+git@gmail.com>
2021-06-03 16:41:48 -0500

    Try inner-photoswipe-gallery

--- assets/js/activate_photoswipe.js
+++ assets/js/activate_photoswipe.js
@@ -1,7 +1,7 @@
 import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js'
 
 const lightbox = new PhotoSwipeLightbox({
-  gallerySelector: '.photoswipe-gallery',
+  gallerySelector: ['.photoswipe-gallery', '.inner-photoswipe-gallery'],
   childSelector: '.photoswipe',
   pswpModule: '/assets/js/photoswipe.esm.min.js',
   pswpCSS: '/assets/css/photoswipe.min.css'
