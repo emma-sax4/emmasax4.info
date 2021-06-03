46c6ecc5497fa6906d562af2acf8c92cf0075243
Emma Sax <emma.sax4+git@gmail.com>
2021-06-03 17:51:00 -0500

    Add caption options

--- assets/js/activate_photoswipe.js
+++ assets/js/activate_photoswipe.js
@@ -1,10 +1,39 @@
 import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js'
 
-const lightbox = new PhotoSwipeLightbox({
+const options = {
   gallerySelector: '.photoswipe-gallery',
   childSelector: '.photoswipe',
   pswpModule: '/assets/js/photoswipe.esm.min.js',
   pswpCSS: '/assets/css/photoswipe.min.css'
+}
+
+const lightbox = new PhotoSwipeLightbox(options)
+
+lightbox.on('uiRegister', function() {
+  lightbox.pswp.ui.registerElement({
+    name: 'custom-caption',
+    order: 9,
+    isButton: false,
+    appendTo: 'root',
+    html: 'Caption Text',
+    onInit: (el, pswp) => {
+      lightbox.pswp.on('change', () => {
+        const currentSlideElement = lightbox.pswp.currSlide.data.element;
+        let captionHTML = '';
+        if (currentSlideElement) {
+          const hiddenCaption = currentSlideElement.querySelector('.hidden-caption-content');
+          if (hiddenCaption) {
+            // get caption from element with class hidden-caption-content
+            captionHTML = hiddenCaption.innerHTML;
+          } else {
+            // get caption from alt attribute
+            captionHTML = currentSlideElement.querySelector('img').getAttribute('alt');
+          }
+        }
+        el.innerHTML = captionHTML || '';
+      });
+    }
+  });
 })
 
 lightbox.init()
